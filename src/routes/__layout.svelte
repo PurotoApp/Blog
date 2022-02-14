d9fe2cbde67b1103d203007b469cfce28776f132
Alex <alexandre.olemans@gmail.com>
2022-02-14 23:43:28 +0100

    attempt to fix OG

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,17 +9,15 @@
 </script>
 
 {#if loading}
-	<div class="absolute h-full w-full bg-background">
+	<div class="absolute h-full w-full bg-background z-50">
 		<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse">
 			<Icon size="6rem" stroke="#DBBE76" />
 		</div>
 	</div>
 {/if}
 
-{#if !loading}
 <Header />
 
 <main class="w-full h-full">
 	<slot />
 </main>
-{/if}
