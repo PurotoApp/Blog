4bf8ab111b3b09876578ccf6d3486c3005d3945f
Alex <alexandre.olemans@gmail.com>
2022-02-25 19:00:10 +0100

    update compareDates script

--- src/lib/compareDates.ts
+++ src/lib/compareDates.ts
@@ -6,16 +6,18 @@ export function compareDates(date: number) {
 	const difference_hours = Math.floor(difference_ms / (1000 * 3600));
 	const difference_minutes = Math.floor(difference_ms / (1000 * 60));
 	if (difference_days >= 7) {
-		// returns DD/MM/YYYY
-		return `${new Date(date).getDate()} ${new Date(date).toLocaleString('default', {
-			month: 'short'
-		})} ${new Date(date).getFullYear()}`;
+		// returns days, months and years
+		return `${new Date(date).toLocaleDateString(undefined, {
+			day: '2-digit',
+			month: 'short',
+			year: '2-digit'
+		})}`;
 	} else if (difference_days > 0) {
-		return `${difference_days}d ago`;
+		return `${difference_days} ${difference_days == 1 ? 'day' : 'days'} ago`;
 	} else if (difference_hours > 0) {
-		return `${difference_hours}h ago`;
+		return `${difference_hours} ${difference_hours == 1 ? 'hour' : 'hours'} ago`;
 	} else if (difference_minutes > 0) {
-		return `${difference_minutes}m ago`;
+		return `${difference_minutes} ${difference_minutes == 1 ? 'minute' : 'minutes'} ago`;
 	} else {
 		return `right now`;
 	}
